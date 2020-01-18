fbdb41a3f9186cc67c1e1960226d7433c706d941
Garrett Luu <garrettluu131@gmail.com>
2020-01-17 17:26:09 -0800

    Moved sketch into proper folder

--- sending/sending.ino
+++ sending/sending.ino
@@ -1,37 +1,43 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST "example.firebaseio.com"
-#define FIREBASE_AUTH "insert-auth-here"
+#define FIREBASE_HOST "fir-esp-41099.firebaseio.com"
+#define FIREBASE_AUTH "xbbCax7dKSjH7WwBFNiU7JtWz7rR8r66XlPbYcsf"
 
-#define WIFI_SSID "example-ssid"
-#define WIFI_PASSWORD "hunter2"
+#define WIFI_SSID "NoFreeHotspot4U"
+#define WIFI_PASSWORD "ONE WORD ALL LOWERCASE"
 
 void setup() {
+  //Begin serial output
   Serial.begin(9600);
 
+  //Attempt to connect to wifi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  
+
+  //If we get here, we are connected!
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
 
+  //start firebase client
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
   //Will repeatedly change the value of "test" field from 3.14 to 6.28 and back
   Firebase.setFloat("test", 3.14);
-
+  delay(1000);
+  Firebase.setFloat("test", 6.28);
   delay(1000);
 
-  Firebase.setFloat("test", 6.28);
-
+  Firebase.set("nested/test", "hello");
+  delay(1000);
+  Firebase.set("nested/test", "world");
   delay(1000);
   
 }
